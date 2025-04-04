3fa0fe587502dd60adbcba94962a9f40feb81ac4
Arun <arunmundakkal003@gmail.com>
2025-04-04 13:42:10 +0530

    Revert "fix: correct spelling of "Docker" in skills data"
    This reverts commit cbb5b6558b0af5d3072188a4e8dc142136eff65b.

--- portfolio/src/All/Skill/Skill.jsx
+++ portfolio/src/All/Skill/Skill.jsx
@@ -26,7 +26,7 @@ const skillsData = [
       { name: "Postman", percentage: 70, website: "https://www.postman.com/" },
       { name: "Firebase", percentage: 80, website: "https://firebase.google.com/" },
       { name: "Vite", percentage: 65, website: "https://vitejs.dev/" },
-      { name: "Docker", percentage: 50, website: "https://www.docker.com/" },
+      { nmae: "Docker", percentage: 50, website: "https://www.docker.com/" },
       { name: "Github", percentage: 70, website: " https://github.com/ "},
       { name: "Git", percentage: 80, website: " https://git-scm.com/ "},
     ],
