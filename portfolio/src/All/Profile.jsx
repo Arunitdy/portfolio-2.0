19b429b324c14235165e55e99f296ba0898c73fa
Arun <99261603+Arunitdy@users.noreply.github.com>
2025-07-06 22:00:07 +0530

    Update Profile.jsx

--- portfolio/src/All/Profile.jsx
+++ portfolio/src/All/Profile.jsx
@@ -110,7 +110,7 @@ export const Profile = () => {
                     </div>
                     <div className="view_conect">
 <a href="/Arun-M-Resume-20250615.pdf" target="_blank" rel="noopener noreferrer">
-  <div className="view">Get Resume</div>
+  <button className="view">Get Resume</button>
 </a>
                         <a href="/Arun-M-Resume-20250615.pdf" download><button className="view">Get Resume</button></a>
                         <a target="_blank" href="https://www.linkedin.com/in/arun-m-8989212aa/"><button className="connect">Connect Me</button></a>
